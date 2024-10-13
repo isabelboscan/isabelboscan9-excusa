5373f39be41f14a08567d5b4a7a2c6ff197b3d15
isabelboscan <isabelboscan9@gmail.com>
2024-10-13 17:43:32 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function dominiogenerador() {
+  let random = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        random += pronoun[i] + adj[a] + noun[b] + ".com<br>";
+      }
+    }
+  }
+  return random;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(dominiogenerador());
+  let generador = document.getElementById("generador");
+  generador.innerHTML = dominiogenerador();
 };
